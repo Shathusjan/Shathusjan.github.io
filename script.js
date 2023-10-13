c28e595deda0029e583092bbd1b39d343ede2ac4
Shathusjan <45368170+Shathusjan@users.noreply.github.com>
2023-10-13 11:04:19 +0530

    Sha-Website

---
+++ script.js
@@ -0,0 +1,7 @@
+function ToggleMenu() {
+    const menu = document.querySelector('.menu_link')
+    const icon = document.querySelector('.hamburger-icon')
+
+    menu.classList.toggle('open')
+    icon.classList.toggle('open')
+}
